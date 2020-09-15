8e36df2c2de50a82a6c72a9fb78b872703bdddcc
[FabianBarrera] <[febarrera12@misena.edu.co]>
2020-09-15 18:02:52 -0500

    Diseño formulario agregar usuario con funcione js

--- vistas/js/plantilla.js
+++ vistas/js/plantilla.js
@@ -5,9 +5,20 @@ SideBar Menu
 $('.sidebar-menu').tree()
 
 /*=============================================
+DATATABLES
+=============================================*/ 
+
+
+/*=============================================
 Inicializar Select2
 =============================================*/
 $('.select2').select2()
 
 //Funcionamiento de Select2 para modal
 $.fn.modal.Constructor.prototype.enforceFocus = function() {};
+
+
+
+
+
+        
--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -24,17 +24,18 @@
       <div class="card">
         <div class="card-header">
 
-          
+
           <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#AgregarUsuario">
-          Agregar usuario
+            Agregar usuario
           </button>
-          
+
         </div>
       </div>
 
       <div class="card-body">
 
-        <table class="table table-bordered table-striped">
+        <table id="example1" class="table table-bordered table-striped">
+        
           <thead>
             <tr>
 
@@ -54,92 +55,174 @@
 
           <tbody>
 
-          <tr>
-            <td>1</td>
-            <td>Fabian Barrera</td>
-            <td>20782</td>
-            <td><img src="vistas/dist/img/avatar5.png" class="img-thumbnail" width="40px"></td>
-            <td>Administrador</td>
-            <td><button class="btn btn-success btn-xs">Activado</button></td>
-            <td>2020-14-09 15:05:32</td>
-            <td>
+            <tr>
+              <td>1</td>
+              <td>Fabian Barrera</td>
+              <td>20782</td>
+              <td><img src="vistas/dist/img/avatar5.png" class="img-thumbnail" width="40px"></td>
+              <td>Administrador</td>
+              <td><button class="btn btn-success btn-xs">Activado</button></td>
+              <td>2020-14-09 15:05:32</td>
+              <td>
 
-            <div class="btn-group">
+                <div class="btn-group">
 
-            <button class="btn btn-warning"><i class="fas fa-pencil-alt"></i></button>
+                  <button class="btn btn-warning"><i class="fas fa-pencil-alt"></i></button>
 
-            <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
 
-            </div>
-            </td>
-          </tr>
+                </div>
+              </td>
+            </tr>
 
-          <tr>
-            <td>2</td>
-            <td>Magreth Angulo</td>
-            <td>20222</td>
-            <td><img src="vistas/dist/img/avatar2.png" class="img-thumbnail" width="40px"></td>
-            <td>Jefe de área TI</td>
-            <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
-            <td>2020-14-09 16:47:29</td>
-            <td>
+            <tr>
+              <td>2</td>
+              <td>Magreth Angulo</td>
+              <td>20222</td>
+              <td><img src="vistas/dist/img/avatar2.png" class="img-thumbnail" width="40px"></td>
+              <td>Jefe de área TI</td>
+              <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
+              <td>2020-14-09 16:47:29</td>
+              <td>
 
-            <div class="btn-group">
+                <div class="btn-group">
 
-            <button class="btn btn-warning"><i class="fas fa-pencil-alt"></i></button>
+                  <button class="btn btn-warning"><i class="fas fa-pencil-alt"></i></button>
 
-            <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
 
-            </div>
-            </td>
-          </tr>
+                </div>
+              </td>
+            </tr>
           </tbody>
 
         </table>
 
       </div>
-   </div>
+  </div>
   </section>
   </div>
 
   <!-- MODAL Agregar Usuario -->
 
-  <!-- Modal -->
-  <div class="modal fade bd-example-modal-xl" tabindex="-1" id="AgregarUsuario" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-  
-<div class="modal-dialog" role="document">
-    
-  <div class="modal-content">
+  <div class="modal fade bd-example-modal-lg" tabindex="-1" id="AgregarUsuario" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
 
-      <div class="modal-header" style="background:#20c997; color:white">
+    <div class="modal-dialog modal-lg" role="document">
 
-        <h5 class="modal-title" id="exampleModalLabel">Agregar usuario</h5>
+      <div class="modal-content">
 
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+        <form role="form" action="#" method="POST" enctype="multipart/form-data">
 
-      </div>
+          <!-- CABEZA DEL MODAL -->
 
-      <div class="modal-body">
-      <div class="box-body">
-      <div class="form-group">
-      <div class="input-group">
-        <span class="input-group-addon"><i class="far fa-user"></i></span>
-        <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Ingresar nombre completo" required>
-      </div>
+          <div class="modal-header" style="background:#20c997; color:white">
 
-      </div>
+            <h5 class="modal-title" id="exampleModalLabel">Agregar usuario</h5>
 
-      </div>
-      </div>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
 
-      <div class="modal-footer">
+          </div>
 
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+          <!-- CUERPO DEL MODAL -->
+
+          <div class="modal-body">
+            <div class="box-body">
+
+              <div class="card-body">
+
+                <div class="row">
+                  <div class="form-group col-md-6 ">
+                    <label for="nombre">Nombre completo</label>
+                    <input type="text" class="form-control" name="nuevoNombre" id="nombre" placeholder="Fabian Enrique Barrera Maldonado" required="">
+                  </div>
+                  <div class="form-group col-md-6 ">
+                    <label for="correo">Email</label>
+                    <input type="email" class="form-control" name="nuevoCorreo" id="correo" placeholder="pasante.ti@vitalis.com.co" required="">
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="form-group col-md-6 ">
+                    <label for="usuario">Usuario SAP</label>
+                    <input type="number" class="form-control" name="nuevoUsuario" id="usuario" placeholder="UsuarioSap" required="">
+                  </div>
+                  <div class="form-group col-md-6 ">
+                    <label for="contraseña">Clave</label>
+                    <input type="password" class="form-control" name="nuevoPassword" id="contraseña" placeholder="Alfa numerica min 8 caracteres" required="">
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="form-group col-md-6 ">
+                    <label for="telefono">Telefono</label>
+                    <input type="text" class="form-control" name="nuevoTelefono" id="telefono" placeholder="3007614919" required="">
+                  </div>
+                  <div class="form-group col-md-6 ">
+                    <label for="area">Área</label>
+                    <select class="form-control" name="nuevaArea" id="area" required="">
+                      <option>Seleccione el área a la que pertenece</option>
+                      <option value="1">Administrativa y Financiera</option>
+                      <option value="2">Comercial</option>
+                      <option value="2">Operaciones</option>
+                      <option value="3">Calidad</option>
+                      <option value="4">Control</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="form-group col-md-6 ">
+                    <label for="perfil">Perfil</label>
+                    <select class="form-control" name="nuevoPerfil" id="perfil" required="">
+                      <option>Seleccione un Rol</option>
+                      <option value="1">Administrador</option>
+                      <option value="2">Gerente</option>
+                      <option value="2">Dirección Financiera</option>
+                      <option value="3">Jefe de Area</option>
+                      <option value="4">Desarrollo Humano</option>
+                    </select>
+                  </div>
+                  <div class="form-group col-md-6 ">
+                    <label for="estado">Estatus</label>
+                    <select class="form-control" name="nuevoEstado" id="estado" required="">
+                      <option>Activar Usuario</option>
+                      <option value="0">Desactivar</option>
+                      <option value="1">Activar</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="form-group col-md-6 ">
+                    <label for="firma">Firma Digital </label><br>
+                    <input type="file" name="firma" id="nuevaFirma" required="">
+                    <small>Peso maximo de la firma 200 MB</small>
+                  </div>
+                  <div class="form-group col-md-6 ">
+                    <label for="foto">Foto </label><br>
+                    <input type="file" name="foto" id="nuevaFoto" required="">
+                    <small>Peso maximo de la foto 200 MB</small>
+                    <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail">
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-body -->
+
+
+            </div>
+          </div>
+
+          <!-- PIE DEL MODAL -->
+
+          <div class="modal-footer justify-content-center">
+
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancelar</button>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+
+          </div>
+        </form>
 
       </div>
     </div>
   </div>
-</div>
